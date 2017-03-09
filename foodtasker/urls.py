1129cd6143fa50c7b9b16cfb05bab93d6c49071d
aca <aca@acas-MacBook-Pro.local>
2017-03-09 19:23:28 +0100

    fb

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -1,6 +1,6 @@
 
 
-from django.conf.urls import url
+from django.conf.urls import url ,include
 from django.contrib import admin
 from foodtaskerapp import views
 from django.contrib.auth import views as auth_views
@@ -13,6 +13,8 @@ urlpatterns = [
     url(r'^restaurant/sign-in', auth_views.login,{'template_name' : 'restaurant/sign_in.html'},name="restaurant-sign-in"),
     url(r'^restaurant/sign-out', auth_views.logout,{'next_page' : '/'},name="restaurant-sign-out"),
     url(r'^restaurant/$', views.restaurant_home, name="restaurant-home"),
-    url(r'^restaurant/sign-up',views.restaurant_sign_up,name="restaurant_sign_up")
+    url(r'^restaurant/sign-up',views.restaurant_sign_up,name="restaurant_sign_up"),
+
+    url(r'^api/social/', include('rest_framework_social_oauth2.urls')),
 
 ]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
